e05f80e37dd8beb41881ea71a8505e18383687ae
jsvdvis <jaapvdvis@gmail.com>
2021-05-27 19:33:18 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,11 @@
 #!/bin/sh -l
 
-composer install --prefer-dist --no-ansi --no-interaction --no-progress --no-scripts
-
 cp .env.example .env
 
 php artisan migrate
+
+composer install --prefer-dist --no-ansi --no-interaction --no-progress --no-scripts
+
 php artisan key:generate
 
 php vendor/bin/phpunit
